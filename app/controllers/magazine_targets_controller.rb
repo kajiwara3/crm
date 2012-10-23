136e3cd90ab661d2b88c5eb2348e8f1b8c4ba570
kajiwara3 <kajiwara3@gmail.com>
2012-10-24 08:11:55 +0900

    magazine_targetコントローラーのページネーションをkaminariに変更中

--- app/controllers/magazine_targets_controller.rb
+++ app/controllers/magazine_targets_controller.rb
@@ -3,9 +3,9 @@ class MagazineTargetsController < ApplicationController
 
   def index
     magazine = MailMagazine.find(params[:mail_magazine_id])
-    @target_list = MagazineTarget.find_all("magazine_id = magazine.id").page(params[:page])
-    # rel = magazine.magazine_targets
-    # @target_list = rel.page(params[:page]).order(:id)
-      #paginate(page: params[:page], per_page: 5)
+    rel = magazine.magazine_targets
+    #@target_list = rel.page params[:page]
+    #@target_list = MagazineTarget.page(params[:page])
+    @target_list = Kaminari.paginate_array(rel).page(params[:page])
   end
 end
--- app/models/magazine_target.rb
+++ app/models/magazine_target.rb
@@ -1,4 +1,4 @@
 class MagazineTarget < ActiveRecord::Base
   belongs_to :mail_magazine
-  paginates_per 5
+
 end
