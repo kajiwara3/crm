a5a0d30cdf0632894f8fbf562df9d933fa86e77a
赤塚 誠二 <Seiji Kajiwara kajiwara@serverworks.co.jp>
2012-10-23 18:35:46 +0900

    コメントアウトした不要なコードを削除した

--- app/controllers/mail_magazines_controller.rb
+++ app/controllers/mail_magazines_controller.rb
@@ -1,9 +1,6 @@
 class MailMagazinesController < ApplicationController
   before_filter :authenticate_user!
   def index
-    #@mail_magazine_list = MailMagazine.order('id desc').
-    #  paginate(page: params[:page], per_page: 5)
-
     @mail_magazine_list = MailMagazine.
       page(params[:page]).order("id desc")
   end
