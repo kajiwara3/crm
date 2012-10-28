01d0dfbef7f645e33f27dc18c93bd3a3ef258db1
kajiwara3 <kajiwara3@gmail.com>
2012-10-28 23:51:19 +0900

    リンクの追加と修正

--- app/controllers/magazine_targets_controller.rb
+++ app/controllers/magazine_targets_controller.rb
@@ -2,8 +2,8 @@ class MagazineTargetsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    magazine = MailMagazine.find(params[:mail_magazine_id])
-    @target_list = Kaminari.paginate_array(magazine.magazine_targets).
+    @magazine = MailMagazine.find(params[:mail_magazine_id])
+    @target_list = Kaminari.paginate_array(@magazine.magazine_targets).
       page(params[:page]).per(5)
   end
 end
