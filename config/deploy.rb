38d086132d5d2f1c7f4aa6fe955c935a236103a9
kajiwara3 <kajiwara3@gmail.com>
2013-11-13 17:01:35 +0900

    staging用のSeedデータを追加した

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ require "rvm/capistrano"
 set :rvm_ruby_strig, "1.9.3"
 
 # ssh settings
-set :use_sudo, false
+set :use_sudo, true
 set :default_run_options, :pty => true
 ssh_options[:forward_agent] = true
 set :normalize_asset_timestamps, false
@@ -29,6 +29,11 @@ namespace :deploy do
   desc "reload the database with seed data"
   task :seed do
     run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  desc "Copy shared config files to current application."
+  task :config_symlink do
+    run "rm -f #{release_path}/config/database.yml; ln -s /var/www/config/database.yml #{release_path}/config/database.yml"
   end
 
   namespace :db do
@@ -71,7 +76,7 @@ end
 after 'deploy:restart', 'unicorn:restart'
 after "deploy:update_code", :bundle_install
 
-
-#require 'capistrano/unicorn'
-#after 'deploy:start', 'unicorn:start'
-#after 'deploy:stop', 'unicorn:stop'
+# 本コマンド実行時に、デプロイ先に配置してあるdatabase.ymlのシンボリックリンクを設定する
+after "deploy:update","deploy:config_symlink"
+# 本コマンド実行時に、デプロイ先に配置してあるdatabase.ymlのシンボリックリンクを設定する
+before "deploy:migrate", "deploy:config_symlink"
